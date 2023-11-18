bfb0fd8ae09825891a4e4afab6734bcb1a3678d4
RomanKovalchik <kovalchykrv@yandex.ru>
2023-11-18 13:59:07 +0300

    adding lesson model

--- course/models.py
+++ course/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Course(models.Model):
-
     title = models.CharField(max_length=255)
     description = models.TextField(null=True)
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    text = models.TextField(null=True)
+    activity = models.TextField(null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'<{self.__class__.__name__}>: {self.title}'
@@ -17,3 +25,4 @@ class Course(models.Model):
     class Meta:
         ordering = ['id']
         db_table = 'course'
+        db_table = 'lesson'
--- course/views.py
+++ course/views.py
@@ -3,6 +3,7 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
 from .models import Course
+from .models import Lesson
 
 
 # Create your views here.
@@ -26,4 +27,14 @@ def course_detail(request: WSGIRequest, course_id: int):
     }
     return render(request, 'course/detail.html', context=context)
 
+def lesson_card(request: WSGIRequest, lesson_id:int):
+    lesson = Lesson.objects.filter(id=1).first()
+    context = {
+         'title': lesson.title,
+         'description':lesson.description,
+         'text': lesson.text,
+         'activity':lesson.activity,
+         'course_id':lesson.course,
+    }
+    return render(request, 'course/lesson_card.html', context=context)
 
