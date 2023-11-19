8b57f6012824a1bc5120a7fd62176138bfc3c887
RomanKovalchik <kovalchykrv@yandex.ru>
2023-11-19 21:24:57 +0300

    adding forms

--- course/urls.py
+++ course/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='course_index'),
     path('<int:course_id>', views.course_detail, name='course_detail'),
-    path('<int:course_id>/<int:lesson_id>', views.lesson_card, name='lesson_card'),
+    path('<int:course_id>/<int:lesson_id>', views.lesson_detail, name='lesson_detail'),
 ]
--- course/views.py
+++ course/views.py
@@ -2,6 +2,7 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
+from .form import LessonForm
 from .models import Course
 from .models import Lesson
 
@@ -21,24 +22,37 @@ def index(request: WSGIRequest):
 
 def course_detail(request: WSGIRequest, course_id: int):
     course = Course.objects.filter(id=course_id).first()
+    if request.method == "POST":
+        form = LessonForm(request.POST)
+
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.course = course
+            form.save()
+
+            form = LessonForm()
+        else:
+            pass
+    else:
+        form = LessonForm()
+
+
     lessons = Lesson.objects.filter(course=course).all()
-    print(course.title)
     context = {
         'title': course.title,
         'course': course,
         'lessons': lessons,
+        'form': form,
     }
     return render(request, 'course/detail.html', context=context)
 
 
-def lesson_card(request: WSGIRequest,course_id, lesson_id:int):
+def lesson_detail(request: WSGIRequest, course_id: int, lesson_id: int):
     lesson = Lesson.objects.filter(id=lesson_id).first()
     context = {
-         'title': lesson.title,
-         'description': lesson.description,
-         'text': lesson.text,
-         'activity': lesson.activity,
-         'course_id': lesson.course,
+        'lesson': lesson,
+        'course_id': course_id,
     }
-    return render(request, 'course/lesson_card.html', context=context)
+    return render(request, 'course/lesson_detail.html', context=context)
+
 
